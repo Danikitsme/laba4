518d5612e4ea98871baa49f6fd4c11f67412eb75
Lenovo <Lenovo@LENOVO-PC63>
2023-12-22 10:30:32 +0200

    Добавьте файлы проекта.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Threading.Tasks;
+
+public class IndexModel : PageModel
+{
+    private readonly IWebHostEnvironment _hostingEnvironment;
+
+    public IndexModel(IWebHostEnvironment hostingEnvironment)
+    {
+        _hostingEnvironment = hostingEnvironment;
+    }
+
+    public void OnGet()
+    {
+        // Ваш код для GET-запиту (якщо потрібно)
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var file = Request.Form.Files["file"];
+
+        if (file != null && file.Length > 0)
+        {
+            var uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "Files");
+
+            // Забезпечте існування директорії для завантаження файлів
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            var filePath = Path.Combine(uploadsFolder, file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            // Додайте ваш код обробки файлу (якщо потрібно)
+
+            return RedirectToPage("/Index");
+        }
+
+        // Обробка помилок, якщо файл не був вибраний
+        ModelState.AddModelError("File", "Виберіть файл для завантаження.");
+        return Page();
+    }
+}
